8ae0d7961cb82aace331896b8d510c3e32f19d2e
Fernando Nicolás <fersanti2896@gmail.com>
2022-10-31 17:26:32 -0600

    Formulario de Registro en Registro.cshtml

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,20 @@
+﻿using ManejoPresupuesto.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers {
+    public class UsuariosController : Controller {
+
+        public IActionResult Registro() {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Registro(RegistroModel modelo) {
+            if (!ModelState.IsValid) { 
+                return View(modelo);
+            }
+
+            return RedirectToAction("Index", "Transacciones");
+        }
+    }
+}
